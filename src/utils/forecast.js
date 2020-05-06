7cdd7935325310375c1c1a5f648a89a8e7fbbb3a
Elliot Kim <elliotkim916@gmail.com>
2020-05-05 18:38:58 -0700

    Updated forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,13 +6,13 @@ const request = require('request');
 const forecast = (lat, lon, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=845a375111434428fa67475af7b7e1f8&query=${lat},${lon}&units=f`;
 
-  request({ url, json: true }, (err, { body : { error, current: { weather_descriptions, temperature, feelslike } } }) => {
+  request({ url, json: true }, (err, { body : { error, current: { weather_descriptions, temperature, feelslike, humidity } } }) => {
     if (err) {
       callback('Unable to connect to weather service!', undefined);
     } else if (error) {
       callback('Unable to find location', undefined);
     } else {
-      callback(undefined, `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees.`);
+      callback(undefined, `Today the weather is ${weather_descriptions[0]}. It is currently ${temperature} degrees out and it should feel like ${feelslike} degrees.  Also, the humidity is currently ${humidity} degrees.`);
     }
   });
 };
